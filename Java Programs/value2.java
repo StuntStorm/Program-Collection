9937e586cc4b4ef2529082f03f78e834eb152afb
Rizwan-Nizarudin <72914440+Rizwan-Nizarudin@users.noreply.github.com>
2022-10-03 19:51:54 +0530

    Add files via upload

---
+++ Java Programs/value2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class value2
+{
+	public static void main(String a[])
+	{    
+        	Scanner sc=new Scanner(System.in);  
+		System.out.print("Enter the Number : ");  
+		int f= sc.nextInt(); 
+		if(f>2)
+		{
+			System.out.print("Enter the Number of time to devide: ");  
+			int s= sc.nextInt(); 
+			int r = s*2;
+			//System.out.println(f%r!=0);
+		}
+		else
+		{
+			System.out.println("The Number is less than 2");
+		}
+		
+			
+		
+		                                                                                                                                                                                              
+    	}  
+	
+}   
