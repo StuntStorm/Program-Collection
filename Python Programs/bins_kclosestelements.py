4e8b89022625e38b3ddbc07f70233e65b41d2f37
a-ma-n <amanali.siddiqui2020@vitstudent.ac.in>
2022-10-09 01:01:00 +0530

    added closest element code

---
+++ Python Programs/bins_kclosestelements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # Initialize binary search bounds
+        left = 0
+        right = len(arr) - k
+        
+        # Binary search against the criteria described
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
